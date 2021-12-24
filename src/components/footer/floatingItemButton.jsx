e8199abce65e3cbf5c3957c96f4e0d1d46d14289
JohnJumon <jhonsonrouslie66@gmail.com>
2021-12-24 23:56:24 +0700

    feat: improve footer 2

--- src/components/footer/floatingItemButton.jsx
+++ src/components/footer/floatingItemButton.jsx
@@ -1,6 +1,6 @@
 const FloatingItemButton = ({icon, onClick}) => {
     return (
-        <div className="w-3/12 h-20 justify-center items-center flex">
+        <div className="w-3/12 h-20 justify-center items-center flex mb-10">
             <img className="bg-white shadow-lg rounded-full p-5 hover:shadow-2xl" src={icon} onClick={onClick}/>
         </div>
     )
--- src/components/footer/footerBar.jsx
+++ src/components/footer/footerBar.jsx
@@ -1,6 +1,6 @@
 const FooterBar = (props) => {
     return (
-        <div className="w-6/12 h-20 flex place-content-between">
+        <div className="w-6/12 h-20 flex place-content-between mt-20">
             {props.children}
         </div>
     )
