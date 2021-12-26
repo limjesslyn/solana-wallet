ecd1f461aa5422d78fa53a420c3e60ebbca857c6
LimJesslyn <jesslynlim.0252@gmail.com>
2021-12-27 01:27:13 +0700

    feat: update box design on home-item

--- src/components/home-item/homeItem.jsx
+++ src/components/home-item/homeItem.jsx
@@ -1,7 +1,13 @@
+import autoprefixer from "autoprefixer"
 
 const HomeItem = () => {
     return (
-        <div className="rounded-2xl bg-white shadow-lg hover:shadow-xl" style={{height: "350px", width:"280px"}}>
+        <div id="content" className="flex mt-10 hover:shadow-xl">
+            <div className="fixed -mt-24 mx-10 w-52 h-52 bg-gray-300 rounded-full"></div>
+            <div className="flex rounded-2xl bg-white shadow-lg items-center" style={{height: "350px", width:"280px"}}>
+                <p className="text-center -mt-10 fixed" style={{width:"280px"}}>Coin</p>
+                <p className="text-center mt-28 fixed" style={{width:"280px"}}>Balance</p>
+            </div>   
         </div>
     )
 }
--- src/components/layout/item.jsx
+++ src/components/layout/item.jsx
@@ -2,13 +2,11 @@ import HomeItem from "../home-item/homeItem";
 const Item = (props) =>{
     return(
         <div className="w-full absolute">
-            <div className="flex flex-row justify-evenly">
-                <span className="ml-auto"><HomeItem/></span>
-                <span className="mt-10"><HomeItem/></span>
-                <span className="mt-20"><HomeItem/></span>
-                <span className="mt-10"><HomeItem/></span>
-                <span className="mr-auto"><HomeItem/></span>
+            <div className="flex flex-row justify-evenly my-20">
+                <span><HomeItem/></span>
+                
             </div>
+            
         </div>
         
     )
