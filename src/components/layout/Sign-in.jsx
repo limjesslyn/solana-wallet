d6bbab274636c63d29607b4508c566595e5afa3c
Nathan Ramli <hezkyanatanael84@gmail.com>
2021-12-27 18:36:41 +0700

    feat: network dropdown

--- src/components/layout/Sign-in.jsx
+++ src/components/layout/Sign-in.jsx
@@ -8,7 +8,20 @@ const Sign = (props) => {
             </NavKiri>
             <NavKanan>
                 <span className="flex item-start py-4 font-bold text-md pl-4 justify-center align-middle">Mainnet</span>
-                <img src={dropdown}/>
+                <div className="dropdown dropdown-end">
+                    <img tabIndex="0" className="btn bg-base-100 border-none hover:bg-base-100" src={dropdown} />
+                    <ul tabindex="0" class="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52">
+                        <li>
+                            <a>Mainnet</a>
+                        </li>
+                        <li>
+                            <a>Devnet</a>
+                        </li>
+                        <li>
+                            <a>Testnet</a>
+                        </li>
+                    </ul>
+                </div>
             </NavKanan>
         </div>
     )
