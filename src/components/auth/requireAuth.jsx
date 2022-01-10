5fd0b3dee4aae63b07acccad2870024b414c82b4
Nathan Ramli <hezkyanatanael84@gmail.com>
2022-01-10 21:40:42 +0700

    fix: require auth component

--- src/components/auth/requireAuth.jsx
+++ src/components/auth/requireAuth.jsx
@@ -1,19 +1,12 @@
-import { useEffect } from "react"
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 const RequireAuth = (props) => {
-    const navigate = useNavigate();
     const isLogin = useSelector(state => state.auth.isLogin);
-    useEffect(() => {
-        if (!isLogin) {
-            navigate("/sign-in", {
-                replace: true
-            })
-        }
-    }, [isLogin]);
 
-    return <>{props.children}</>
+    return <>{
+        !isLogin ?
+            <Navigate to={"/sign-in"} replace={true} /> : props.children}</>
 }
 
 export default RequireAuth
